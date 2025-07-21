72a0d4874f7c05085ae361bbe64baf07e12c3cc5
seia1007 <1007seia@gmail.com>
2025-07-21 11:57:11 +0900

    Update script.js

--- 整体院サイト/script.js
+++ 整体院サイト/script.js
@@ -1,10 +1,13 @@
 // メイン画像カルーセル
-$('.carousel').slick( {
-  autoplay:true,
-  dots: true,
-  infinite: true,
-  speed:1000,
-  autoplaySpeed: 5000,
-  fade: true,
-  cssEase: 'linear'
+$(document).ready(function () {
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false
+  });
 });
